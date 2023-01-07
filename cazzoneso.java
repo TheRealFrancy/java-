ee47e777124ed1fc71b904e6fdc35ffb45a1ff16
TheRealFrancy <38110096+TheRealFrancy@users.noreply.github.com>
2023-01-07 22:15:35 +0100

    Create cazzoneso.java

---
+++ cazzoneso.java
@@ -0,0 +1,60 @@
+import javax.swing.JOptionPane;
+        public class Main {
+            public static void main(String[] args) {
+
+                String scelta;
+                int contofrancesco = 0;
+                String[] options = {"aggiungi", "rimuovi", "cancella"};
+                int action;
+                boolean continuare = true;
+                boolean continuaredue = true;
+                JOptionPane.showMessageDialog(null, "Ciao benvnut* nel cringe bank system\n digita a per visualizzare il tuo saldo\n digita b per aggiungere o rimuovere soldi dal tuo conto");
+
+                while (continuaredue) {
+                    scelta = JOptionPane.showInputDialog("Scegli l'opzione");
+                    if (scelta.equals("a")) {
+                        JOptionPane.showMessageDialog(null, "il conto e di" + contofrancesco);
+
+                    } // end if a
+
+
+                    if (scelta.equals("b")) {
+                        while (continuare) {
+                            action = JOptionPane.showOptionDialog(null, "finestra di modifica soldi", "il tuo saldo e di:" + contofrancesco, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+
+                            if (action == JOptionPane.YES_OPTION) {
+                                contofrancesco = contofrancesco + 100;
+
+
+                            } else if (action == JOptionPane.NO_OPTION) {
+                                contofrancesco = contofrancesco - 100;
+
+                            } else if (action == JOptionPane.CANCEL_OPTION) {
+                                continuare = false;
+                                //JOptionPane.showMessageDialog(null, "il conto e di" + contofrancesco);
+
+
+                            }
+
+                        }
+
+
+                    } // end if b
+
+
+
+
+                    /*   double capitale = 1000.0; // il capitale iniziale, in dollari
+                double tassoInteresse = 0.05; // il tasso di interesse del 5%
+                int numeroAnni = 10; // calcoliamo gli interessi per 10 anni
+                for(int i =1; i<numeroAnni; i++){
+
+                   double interessexanno= capitale * tassoInteresse ;
+                   System.out.println("Gli interessi dell'anno"+i+"sono"+interessexanno);
+                   capitale+=interessexanno;
+    }*/
+            continuare=true;
+                }
+
+            }// end main
+        } //end class
